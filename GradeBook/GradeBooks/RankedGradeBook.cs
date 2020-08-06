f435fd2004b8a94a127cebd75f2bd02ab6bcbc98
imaltont <sondrehegdal@protonmail.com>
2020-08-06 09:44:34 +0200

    module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -43,7 +43,7 @@ namespace GradeBook.GradeBooks
         {
             if (this.Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 students.");
                 return;
             }
             base.CalculateStudentStatistics(name);
