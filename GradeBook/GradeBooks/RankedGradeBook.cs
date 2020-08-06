bdf8f0b9c591c71e67a7c1e8613a9c25603ade13
imaltont <sondrehegdal@protonmail.com>
2020-08-06 09:32:22 +0200

    module 2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using GradeBook.Enums;
 namespace GradeBook.GradeBooks
 {
@@ -6,6 +8,36 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             this.Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (this.Students.Count < 5)
+            {
+                throw new System.InvalidOperationException();
+            }
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e=> e.AverageGrade).ToList();
+            if (averageGrade >= grades[threshold - 1])
+            {
+                return 'A';
+            }
+
+            if (averageGrade >= grades[threshold * 2 - 1])
+            {
+                return 'B';
+            }
+
+            if (averageGrade >= grades[threshold * 3 - 1])
+            {
+                return 'C';
+            }
+
+            if (averageGrade >= grades[threshold * 4 - 1])
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
